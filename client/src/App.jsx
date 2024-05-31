7e6ff2d072d370d807adf637fa2c07a88ae6893b
Paweł Dowgiało <pawel.dowgialo@cyfroweszkoly.pl>
2024-05-31 23:18:54 +0200

    Add server and modify main package.json

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import AddUserForm from './components/AddUserForm'
+import UserList from './components/UserList'
+import {Routes, Route, Link, useNavigate} from 'react-router-dom'
+
+function App() {
+    const navigate = useNavigate()
+
+  
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
